004d186caba92e015f7a8324e837477f10ab4976
faizan0209 <muhammadfaizansajid0@gmail.com>
2025-01-25 22:41:37 +0500

    ook

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ require("dotenv").config();
 require("./Models/db");
 const bodyParser = require("body-parser");
 const AuthRouter = require("./Models/Routes/AuthRouter");
-const productRouter = require("./Models/Routes/productRouter");
-const messageRoutes = require("./Models/Routes/messages");
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(app);
@@ -26,20 +24,7 @@ app.post("/", (req, res) => {
 });
 
 app.use("/auth", AuthRouter);
-app.use("/products", productRouter);
-app.use("/api/messages", messageRoutes);
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("sendMessage", (msg) => {
-    io.emit("receiveMessage", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
